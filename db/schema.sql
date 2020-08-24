543e0dca2b13ce737b890c8abb61d42b47abc2d9
Michael Portu <michaelportu@Michaels-MacBook-Pro.local>
2020-08-23 20:28:17 -0500

    department table created

--- db/schema.sql
+++ db/schema.sql
@@ -9,7 +9,7 @@ CREATE TABLE department (
 );
 
 CREATE TABLE role (
-    id INTEGER NOT NULL,
+    id INTEGER AUTO_INCREMENT NOT NULL,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INTEGER,
@@ -18,7 +18,7 @@ CREATE TABLE role (
 );
 
 CREATE TABLE employee (
-    id INTEGER NOT NULL,
+    id INTEGER AUTO_INCREMENT NOT NULL,
     first_name VARCHAR(30) NOT NULL, 
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER NOT NULL,
